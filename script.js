eaebc751762f716d78594952d1c5ffc8c0191671
theTai123 <lamgiatai381@gmail.com>
2023-07-17 12:35:42 +0700

    add backgound color

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
-let nav = document.getElementById("nav");
-let text =document.getElementById("text");
-let btn = nav.children[1];
+var dark = document.querySelector(".dark");
+var white = document.querySelector(".white");
+var rainbow = document.querySelector(".rainbow");
+var btn = document.getElementById("color").querySelectorAll("button");
+var boxChat = document.querySelector(".box-chat");
+var wrapper = document.querySelector(".wrapper");
 
-console.log(btn);
-
-window.onload = ()=>{
-    text.style.display="none";
+function addActive(){
+    btn.forEach((item) => item.classList.remove("active"));
+    this.classList.add("active");
 }
 
-btn.addEventListener("click", () => {
-    text.style.display="block";
-   
-    let newBtn =document.createElement('button');
-    newBtn.innerText="click me";
-    newBtn.addEventListener("click",()=>{
-        alert("Hello");
-    });
+btn.forEach((item) => item.addEventListener("click", addActive));
 
-    if(text.querySelector('button')!==null) return;
-    text.appendChild(newBtn);
+dark.addEventListener("click", ()=>{
+    wrapper.classList.remove(wrapper.classList[1]);
+    wrapper.classList.add("dark");
+    boxChat.classList.add("white");
 });
 
+white.addEventListener("click", ()=>{
+    wrapper.classList.remove(wrapper.classList[1]);
+    wrapper.classList.add("white");
+    boxChat.classList.remove(boxChat.classList[1]);
+});
 
-
-
-
+rainbow.addEventListener("click", ()=>{
+    wrapper.classList.remove(wrapper.classList[1]);
+    wrapper.classList.add("rainbow");
+    boxChat.classList.remove(boxChat.classList[1]);
+});
